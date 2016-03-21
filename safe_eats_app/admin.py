f800f866a321cc0a0e8595f5fd1015c7c1465b45
Ron <ronshafii2@gmail.com>
2016-03-21 16:11:48 -0700

    updated models and added return string functions

--- safe_eats_app/admin.py
+++ safe_eats_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import RestaurantInfo, InspectionReport, InspectionResult
+from .models import RestaurantInfo, InspectionReport, InspectionResult, HealthInspectionSource
 
 # Register your models here.
 admin.site.register(RestaurantInfo)
 admin.site.register(InspectionReport)
 admin.site.register(InspectionResult)
+admin.site.register(HealthInspectionSource)
--- safe_eats_app/models.py
+++ safe_eats_app/models.py
@@ -5,17 +5,19 @@ from django.db import models
 class RestaurantInfo(models.Model):
 	"""Describes data saved in Restaurant Info table"""
 
-	business_id = models.CharField(max_length=15)
-	google_restaurant_id = models.CharField(max_length=100)
-	yelp_restaurant_id = models.CharField(max_length=100)
+	business_id = models.SlugField(max_length=15)
+	google_restaurant_id = models.CharField(max_length=100, blank=True)
+	yelp_restaurant_id = models.CharField(max_length=100, blank=True)
 	business_name = models.CharField(max_length=100)
 	address = models.CharField(max_length=100)
 	city = models.CharField(max_length=50)
-	zip_code = models.CharField(max_length=15)
+	zip_code = models.SlugField(max_length=15)
 	longitude = models.FloatField()
 	latitude = models.FloatField()
-	health_inspection_src = models.CharField(max_length=100)
+	health_inspection_src = models.ForeignKey('HealthInspectionSource')
 
+	def __str__(self):
+		return self.business_name
 
 
 
@@ -23,32 +25,36 @@ class RestaurantInfo(models.Model):
 class InspectionReport(models.Model):
 	"""Describes the Inspection Report ID"""
 
-	restaurant = models.ForeignKey(RestaurantInfo)
-	inspection_serial_num = models.CharField(max_length=15)
+	restaurant = models.ForeignKey('RestaurantInfo')
+	inspection_serial_num = models.SlugField(max_length=15)
 	inspection_business_name = models.CharField(max_length=100)
-	inspection_date = models.CharField(max_length=30)
+	inspection_date = models.DateField()
 
-
+	def __str__(self):
+		return self.inspection_business_name
 
 
 
 class InspectionResult(models.Model):
 	"""Describes data saved in the Inspection Report table"""
 
-	inspection = models.ForeignKey(InspectionReport)
+	inspection = models.ForeignKey('InspectionReport')
 	inspection_score = models.CharField(max_length=3)
 	inspection_type = models.CharField(max_length=100)
 	inspection_result = models.CharField(max_length=15)
-	violation_description = models.CharField(max_length=15)
+	violation_description = models.CharField(max_length=15, blank=True)
 	violation_points = models.CharField(max_length=15)
-	violation_record_id = models.CharField(max_length=15)
+	violation_record_id = models.CharField(max_length=15, blank=True)
 
 
 
 class HealthInspectionSource(models.Model):
-	"""Describes the source for each municipality"""
+	"""Describes the source of data used for each municipality"""
 
 	name = models.CharField(max_length=100)
-	website = models.CharField(max_length=100)
+	website = models.URLField(max_length=100)
 	phone = models.CharField(max_length=100)
-	email = models.CharField(max_length=100)
+	email = models.EmailField(max_length=100, blank=True)
+
+	def __str__(self):
+		return self.name
