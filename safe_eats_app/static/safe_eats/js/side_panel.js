73295a9dd554672ecc898c2424df1dcf8ccee495
Ron <ronshafii2@gmail.com>
2016-04-06 14:31:47 -0700

    working restaurant reports slide panel

--- safe_eats_app/static/safe_eats/js/side_panel.js
+++ safe_eats_app/static/safe_eats/js/side_panel.js
@@ -1,17 +1,35 @@
 
 
-function slidePanel(bus_id){
-	$.ajax({
-        url: "/restaurants/" + bus_id, 
-        success: function(data){
-            $("#slide-panel").html(data); 
-        }
-    });
+function slidePanel(bus_id) {
+    if (bus_id === slidePanelId){
+        slide();
+    } else {
 
+        $.ajax({
+            url: "/restaurants/" + bus_id,
+            success: function(data){
+                var panel = $('#slide-panel');
+                panel.html(data);
+
+                slidePanelId = bus_id;
+                if (panel.hasClass("visible")) {
+
+                } else {
+                    slide();
+                }
+                
+            }
+        });
+    }
+    
+}
+
+function slide(){
     var panel = $('#slide-panel');
-	if (panel.hasClass("visible")) {
-		panel.removeClass('visible').animate({'margin-right':'-300px'});
-	} else {
-		panel.addClass('visible').animate({'margin-right':'0px'});
-	}
+
+    if (panel.hasClass("visible")) {
+        panel.removeClass('visible').animate({'margin-right':'-300px'}, function() {panel.addClass("hidden");});
+    } else {
+        panel.removeClass("hidden").animate({'margin-right':'0px'}, function() {panel.addClass('visible');});
+    }
 }
