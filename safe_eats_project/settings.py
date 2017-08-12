38128fb20713a5070affa5bc4560ea82a7399e50
Ron Shafii <ronshafii2@gmail.com>
2017-08-12 11:43:40 -0700

    comenting out local settings

--- safe_eats_project/settings.py
+++ safe_eats_project/settings.py
@@ -5,7 +5,7 @@ Django settings for safe_eats_project project.
 
 import os
 import dj_database_url
-from .local_settings import *
+# from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
