d273b15d76b87167eb1cc8f4d8542160cd961d10
Ron Shafii <ronshafii2@gmail.com>
2017-08-16 15:44:58 -0700

    updated line 22 and commented 23&24

--- safe_eats_project/settings.py
+++ safe_eats_project/settings.py
@@ -19,9 +19,9 @@ STATICFILES_DIRS = (
 
 
 
-# DATABASES['default'] = dj_database_url.config()
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #       'default': dj_database_url.config(
