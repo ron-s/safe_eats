339739b0c34736d07e5427834f81ba1637e48443
Ron Shafii <ronshafii2@gmail.com>
2017-08-16 16:04:06 -0700

    appending .local to

--- safe_eats_project/wsgi.py
+++ safe_eats_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "safe_eats_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "safe_eats_project.settings.local")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
