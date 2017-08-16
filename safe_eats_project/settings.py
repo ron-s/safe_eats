796c3373d252c884e3af45fe398926b91884d934
Ron Shafii <ronshafii2@gmail.com>
2017-08-16 15:55:37 -0700

    added DATABASES line 21

--- safe_eats_project/settings.py
+++ safe_eats_project/settings.py
@@ -18,8 +18,8 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 
-
-DATABASES['default'] = dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
+# DATABASES['default'] = dj_database_url.config()
 # db_from_env = dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(db_from_env)
 
